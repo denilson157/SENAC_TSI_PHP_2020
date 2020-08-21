19f8527f339c35caaf4718f13a38b70c65f75910
Denílson Pereira <deene67@gmail.com>
2020-08-20 22:12:47 -0300

    aula de operadores

--- operadores.php
+++ operadores.php
@@ -1,2 +1,13 @@
 <?php
 
+$saldo = 55.55; //operador de atribuição
+
+echo "<br> o seu saldo é $saldo";
+
+$novoSaldo = $saldo - 10; //operador de subtração
+
+echo "<br> depois de remover 10, seu novo saldo é $novoSaldo";
+
+$novoNovoSaldo = $novoSaldo * 23;
+
+echo "<br> depois de remover 10, multiplicamos 23, e seu novo saldo é $novoNovoSaldo";
