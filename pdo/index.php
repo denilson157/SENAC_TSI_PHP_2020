346bfb7bc9f801a88aa33ad4b177e3db9905d2b2
DENILSON PEREIRA <deene67@gmail.com>
2020-10-29 21:21:33 -0300

    meio da aula

--- pdo/index.php
+++ pdo/index.php
@@ -6,14 +6,61 @@ error_reporting(E_ALL);
 
 
 $dsn = 'sqlsrv:Server=localhost\\SQLEXPRESS;Database=PHP';
-$user = 'denilson';
+$user = 'sa';
 $password = 'A3154868e@';
 
 
 $db = new PDO($dsn, $user, $password);
 
-$sql = "SELECT nome, telefone FROM cliente";
+$sql = 'SELECT nome, telefone FROM cliente';
 
-foreach ($db->query($sql) as $registro) {
-    echo "Nome: {$registro['nome']} Telefone: {$registro['telefone']}\n<br>";
+$r = $db->query($sql);
+git config --global user.email "deene67@gmail.com"
+    git config --global user.name "denilson157"
+echo '<pre>';
+
+while ($reg = $r->fetch(PDO::FETCH_ASSOC)) {
+
+	echo "Sr(a) {$reg['nome']}, seu telefone {$reg['telefone']} será bloqueado se a conta não for paga \n\n";
+}
+
+//Como fazer um DELETE
+if ( $db->query('DELETE FROM cliente WHERE id = 2') ) {
+
+	echo "ID 2 apagado com sucesso!\n\n";
+
+} else {
+
+	echo "Erro ao tentar apagar o ID 2";
+}
+
+
+//Como fazer um UPDATE
+if ( $db->query('UPDATE cliente SET nome = "Arthur" WHERE id = 3') ) {
+
+	echo "ID 3 atualizado com sucesso!\n\n";
+
+} else {
+
+	echo "Erro ao tentar atualizar o ID 3";
+}
+
+
+//Como fazer um INSERT
+if ( $db->query('	INSERT INTO cliente 
+						(id, nome, telefone) 
+					VALUES 
+						(2, "Luiz Bono", 5511994586373)') ) {
+
+	echo "ID 2 gerado novamente com sucesso!\n\n";
+
+} else {
+
+	echo "Erro ao tentar gerar novamente o ID 2";
+}
+
+//Como fazer um SELECT
+foreach ( $db->query($sql) as $reg ) {
+
+	echo "Sr(a) {$reg['nome']}, seu telefone {$reg['telefone']} será bloqueado se a conta não for paga \n\n";
 }
